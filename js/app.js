8e4b0b0a8bcba206cd44040bb5cf9d77cc76538c
kevlewis <mrlewis19@gmail.com>
2016-07-02 18:42:35 -0400

    Instamax

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(function() {
         sortBy: 'most-recent',
         resolution: 'standard_resolution',
         template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>',
-        after: function(){}
+        
     });
     feed.run();
  
